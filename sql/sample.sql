0322b3f68b55895981db4d75e98465f1b225c4fc
Przemysław Broniszewski <przemek@broniszewski.net>
2019-02-28 20:53:41 +0100

    update db sample tables

--- sql/sample.sql
+++ sql/sample.sql
@@ -1,20 +1,42 @@
 -- Sample tables for sql-stream-queue
 
-CREATE TABLE IF NOT EXISTS `ssq_stream` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `stream_name` varchar(64) NOT NULL,
-  `message` text NOT NULL,
-  `created_at` datetime NOT NULL,
+CREATE TABLE IF NOT EXISTS `ssq_stream`
+(
+  `id`                int(11)     NOT NULL AUTO_INCREMENT,
+  `stream_name`       varchar(64) NOT NULL,
+  `message`           text        NOT NULL,
+  `created_at`        datetime    NOT NULL,
   `planned_execution` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+  PRIMARY KEY (`id`),
+  KEY `stream_name` (`stream_name`),
+  KEY `planned_execution` (`planned_execution`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  AUTO_INCREMENT = 1;
 
-CREATE TABLE IF NOT EXISTS `ssq_status` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `stream_id` int(11) NOT NULL,
-  `consumer_group` varchar(64) NOT NULL,
-  `status` enum('NEW','PENDING','ACK','') NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+CREATE TABLE IF NOT EXISTS `ssq_status`
+(
+  `id`                int(11)                      NOT NULL AUTO_INCREMENT,
+  `stream_id`         int(11)                      NOT NULL,
+  `consumer_group_id` int(11)                      NOT NULL,
+  `status`            enum ('NEW','PENDING','ACK') NOT NULL,
+  `created_at`        datetime                     NOT NULL,
+  `updated_at`        datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `stream_id` (`stream_id`),
+  KEY `consumer_group_id` (`consumer_group_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  AUTO_INCREMENT = 1;
+
+CREATE TABLE IF NOT EXISTS `ssq_consumer_group`
+(
+  `id`        int(11)     NOT NULL AUTO_INCREMENT,
+  `stream_id` int(11)     NOT NULL,
+  `name`      varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `consumerGroup` (`stream_id`, `name`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  AUTO_INCREMENT = 1;
+
