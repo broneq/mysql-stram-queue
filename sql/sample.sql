9c3086dbca45ab0e27d0b3125a055f32368e7db4
Przemysław Broniszewski <przemek@broniszewski.net>
2019-02-28 19:19:56 +0100

    sample SQL table

--- sql/sample.sql
+++ sql/sample.sql
@@ -1,1 +1,20 @@
 -- Sample tables for sql-stream-queue
+
+CREATE TABLE IF NOT EXISTS `ssq_stream` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stream_name` varchar(64) NOT NULL,
+  `message` text NOT NULL,
+  `created_at` datetime NOT NULL,
+  `planned_execution` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `ssq_status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stream_id` int(11) NOT NULL,
+  `consumer_group` varchar(64) NOT NULL,
+  `status` enum('NEW','PENDING','ACK','') NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
