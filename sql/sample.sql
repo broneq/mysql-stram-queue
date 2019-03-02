c14e87503ded7b9c035606865040554d01a2f54e
Przemysław Broniszewski <przemek@broniszewski.net>
2019-03-02 13:58:08 +0100

    fix db structure

--- sql/sample.sql
+++ sql/sample.sql
@@ -1,14 +1,25 @@
 -- Sample tables for sql-stream-queue
 
+
 CREATE TABLE IF NOT EXISTS `ssq_stream`
 (
-  `id`                int(11)     NOT NULL AUTO_INCREMENT,
-  `stream_name`       varchar(64) NOT NULL,
-  `message`           text        NOT NULL,
-  `created_at`        datetime    NOT NULL,
+  `id`   int(11)     NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  AUTO_INCREMENT = 1;
+
+CREATE TABLE IF NOT EXISTS `ssq_stream_queue`
+(
+  `id`                int(11)  NOT NULL AUTO_INCREMENT,
+  `stream_id`         int(11)  NOT NULL,
+  `message`           text     NOT NULL,
+  `created_at`        datetime NOT NULL,
   `planned_execution` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `stream_name` (`stream_name`),
+  KEY `stream_id` (`stream_id`),
   KEY `planned_execution` (`planned_execution`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8
@@ -17,13 +28,13 @@ CREATE TABLE IF NOT EXISTS `ssq_stream`
 CREATE TABLE IF NOT EXISTS `ssq_status`
 (
   `id`                int(11)                      NOT NULL AUTO_INCREMENT,
-  `stream_id`         int(11)                      NOT NULL,
+  `stream_queue_id`   int(11)                      NOT NULL,
   `consumer_group_id` int(11)                      NOT NULL,
   `status`            enum ('NEW','PENDING','ACK') NOT NULL,
   `created_at`        datetime                     NOT NULL,
   `updated_at`        datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `stream_id` (`stream_id`),
+  KEY `stream_queue_id` (`stream_queue_id`),
   KEY `consumer_group_id` (`consumer_group_id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8
